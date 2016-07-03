71632e4016f087c0897ed96647243f334b24e9fd
Nishant Srivastava <nisrulz@gmail.com>
2016-07-03 15:31:47 +0530

    added library code to app

--- app/src/main/java/github/nisrulz/sample/myapplication/MainActivity.java
+++ app/src/main/java/github/nisrulz/sample/myapplication/MainActivity.java
@@ -1,12 +1,15 @@
 package github.nisrulz.sample.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import github.nisrulz.awesomelibrary.AwesomeLibrary;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    AwesomeLibrary.getInstance().makeMeAwesome(this, "Nishant");
   }
 }
